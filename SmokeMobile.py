7df81b57c7d80c2168d97cf8eb27aa6aa03d641d
carlos <carlos2603.cc@gmail.com>
2022-03-04 09:00:04 -0300

    limpieza de comentarios

--- SmokeMobile.py
+++ SmokeMobile.py
@@ -24,7 +24,7 @@ class Smoke_Mobile(unittest.TestCase):
         self.pas="1234"
         self.wait = WDW(self.driver, 100)
     #esto es una prueba
-        #showLibraryContents
+        #showLibraryContentsgit
 
         #login
         self.driver.find_element(By.ID, "ar.com.unisolutions.unigis_deliveries_x:id/email").send_keys(self.user)
